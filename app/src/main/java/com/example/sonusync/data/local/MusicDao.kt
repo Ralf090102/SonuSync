60d447640ee5af54bbeed132e20fb8c3d7ed84a3
Ralf090102 <ralf.hernandez090102@gmail.com>
2024-10-14 11:48:13 +0800

    MusicDao & MusicRepo Finished

---
+++ app/src/main/java/com/example/sonusync/data/local/MusicDao.kt
@@ -0,0 +1,23 @@
+package com.example.sonusync.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.sonusync.domain.model.Music
+
+
+@Dao
+interface MusicDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(musicList: List<Music>)
+
+    @Query("SELECT * FROM music")
+    suspend fun getAllMusic(): List<Music>
+
+    @Query("SELECT * FROM music WHERE title LIKE :title LIMIT 1")
+    suspend fun findMusicByTitle(title: String): Music
+
+    @Query("DELETE FROM music WHERE id = :musicId")
+    suspend fun deleteMusic(musicId: Long)
+}
--- app/src/main/java/com/example/sonusync/di/NetworkModule.kt
+++
@@ -1,4 +0,0 @@
-package com.example.sonusync.di
-
-class NetworkModule {
-}
