9f6312170a127ee3afccedcccf638db45f87842a
Ralf090102 <ralf.hernandez090102@gmail.com>
2024-10-14 13:37:25 +0800

    Small Comment

--- app/src/main/java/com/example/sonusync/SonuSync.kt
+++ app/src/main/java/com/example/sonusync/SonuSync.kt
@@ -3,6 +3,7 @@ package com.example.sonusync
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//Global Libraries or 3rd Party Dependencies Goes Here
 @HiltAndroidApp
 class SonuSync : Application() {
 
